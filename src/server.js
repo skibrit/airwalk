a6e9218db63f3ad9252b008a26e32e97529a1ddb
Saidul Amin <saidulnobin@gmail.com>
2023-08-25 05:21:45 +0600

    initial commit

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+// const dbConnecter = require("../config/dbConnecter");
+// const routeList = require("./routeList");
+const PORT = process.env.PORT || 6000;
+
+//create a connection to database
+// dbConnecter();
+
+//add middleware parse
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ extended: false }));
+
+// attach routes
+// app.use("/api/user", routeList.User);
+// app.use("/api/post", routeList.Post);
+
+app.get("/", function (req, res) {
+  res.send("Welcome to Microservice of Airwalk");
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
